5852a7edd3cfa612e3e1e59bb62cce9718dcbe5b
Xbadiam1 <93679795+Xbadiam1@users.noreply.github.com>
2021-11-08 19:51:04 +0100

    Update scraper.py

--- src/scraper.py
+++ src/scraper.py
@@ -2,7 +2,6 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import pandas as pd
 import csv
-import re 
 import os
 
 #-------------------------------------------------------------------------------------------------
